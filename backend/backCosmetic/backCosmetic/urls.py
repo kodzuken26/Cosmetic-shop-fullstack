efb38a8b86050b033a50c2c8e8d35db350a63cd5
Kristina Borisova <kodzuken26@yandex.ru>
2026-04-23 23:32:19 +0400

    changes urls.py in backCosmetic2

--- backend/backCosmetic/backCosmetic/urls.py
+++ backend/backCosmetic/backCosmetic/urls.py
@@ -1,35 +1,20 @@
-"""
-URL configuration for backCosmetic project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import serve_react_app
 
-from backCosmetic import settings
-from django.conf.urls.static import static
-# from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('admin/', admin.site.urls),          # 1. Админка Django
+    path('api/', include('api.urls')),        # 2. Твои API-эндпоинты
+    path('api-auth/', include('rest_framework.urls')), # 3. Для DRF
 ]
 
+# 4. Для медиа-файлов (картинки товаров)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [re_path(r'^.*$', serve_react_app, name='react_app')]
+# 5. ВСЕ остальные запросы отдаем React
+urlpatterns += [
+    re_path(r'^.*$', serve_react_app, name='react_app'),
+]
